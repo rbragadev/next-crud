565911f46ee62f433b00768ffa9746ed33508926
Raphael Braga <raffael.info@gmail.com>
2024-02-20 15:37:30 -0300

    :rocket:feat: implementando metodos firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,41 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import Layout from '@/components/Layout';
 import Tabela from '@/components/Tabela';
 import Cliente from '@/core/Cliente';
 import Botao from '@/components/Botao';
 import Formulario from '@/components/Formulario';
-import { useState } from 'react';
-
-const inter = Inter({ subsets: ['latin'] });
+import { useEffect, useState } from 'react';
+import ClienteRepositorio from '@/core/ClienteRepositorio';
+import ColecaoCliente from '@/backend/db/ColecaoCliente';
 
 export default function Home() {
+  const repo: ClienteRepositorio = new ColecaoCliente();
+
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio());
+  const [clientes, setClientes] = useState<Cliente[]>([]);
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela');
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 33, '2'),
-    new Cliente('Carlos', 23, '3'),
-    new Cliente('Pedro', 54, '4'),
-  ];
+  useEffect(obterTodos, []);
+
+  function obterTodos() {
+    repo.obterTodos().then((clientes) => {
+      setClientes(clientes);
+      setVisivel('tabela');
+    });
+  }
 
   function clienteSelecionado(cliente: Cliente) {
     setCliente(cliente);
     setVisivel('form');
   }
 
-  function clienteExcluido(cliente: Cliente) {
-    console.log(cliente.nome);
+  async function clienteExcluido(cliente: Cliente) {
+    repo.excluir(cliente);
+    obterTodos();
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente);
-    setVisivel('tabela');
+  async function salvarCliente(cliente: Cliente) {
+    repo.salvar(cliente);
+    obterTodos();
   }
 
   function novoCliente() {
